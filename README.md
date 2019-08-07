51b28e6cc1adc4bf2f7a1b9c7d51089ccc03a2c9
cosimocollini <cosicollini@gmail.com>
2019-08-07 16:39:23 +0200

    merge optimization split configurations

---
+++ README.md
@@ -0,0 +1,16 @@
+# Webpack basic configuration
+
+# What i learned
+
+It took me a while to really understand how a configuration like this works behind the scenes, but after a bit of research, I can manage my configuration based on the project I'm developing.
+For now it's just a base but it's already very useful for managing my code from development to production.
+
+## Resources
+
+- [Webpack Documentation](https://webpack.js.org/concepts/)
+
+I found some videos on YouTube where different configurations are explained step by step, which helped me a lot to understand the various Webpack concepts:
+
+- [DesignCourse - Gary Simon](https://www.youtube.com/watch?v=TzdEpgONurw&t=0s)
+- [Chris Courses](https://www.youtube.com/watch?v=aYo7YymudpE&list=PLpPnRKq7eNW2_dy8Re4lyByThMtTAhw5Q)
+- [Colt Steele](https://www.youtube.com/watch?v=3On5Z0gjf4U&list=PLblA84xge2_zwxh3XJqy6UVxS60YdusY8)
---
+++ webpack.dev.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.join(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new BrowserSyncPlugin({
+      notify: false,
+      host: "0.0.0.0",
+      port: 3000,
+      proxy: "http://localhost:8080/",
+      files: [
+        {
+          match: ["**/*.html"],
+          fn: function(event, file) {
+            if (event === "change") {
+              const bs = require("browser-sync").get("bs-webpack-plugin");
+              bs.reload();
+            }
+          }
+        }
+      ]
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  }
+});
